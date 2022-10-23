c7a8c356206cde6f6c487a8cc4cc5056c28e73f7
marcoabensur <marco.abensur@gmail.com>
2022-10-23 09:17:37 -0300

    Fix compilation issue for target uC

--- inc/bsp.h
+++ inc/bsp.h
@@ -44,7 +44,7 @@ enum {
 }; /* number of clock ticks in a second */
 
 typedef enum {
-   SIMULATOR = QS_USER0,
+   SIMULATOR = 100, //QS_USER0 on qs.h file. only useful on Qspy config
 } qs_user_names_t;
 
 
