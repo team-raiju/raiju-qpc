a7fac9f1e2687d93aa482f09f7e34489792837fe
marcoabensur <marco.abensur@gmail.com>
2023-01-25 23:27:02 -0300

    fiz eeprom initialization

--- src/main.c
+++ src/main.c
@@ -48,6 +48,7 @@ int main() {
 
     QF_init();  /* initialize the framework */
     BSP_init(); /* initialize the BSP */
+    sumo_parameters_init();
     APP_init();
 
     /* instantiate and start the SumoHSM active object */
